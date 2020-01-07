6dcbee47aa2b1d134023248393b0e28cad85717a
lemon <a8262589>
2020-01-07 22:03:49 +0800

    Chinese dialogpt v1, train

--- od/utils/data_utils.py
+++ od/utils/data_utils.py
@@ -28,3 +28,4 @@ def save_json(data, path, indent=0):
     with open(path, 'w', encoding='UTF-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=indent)
 
+from pytorch_pretrained_bert import OpenAIGPTTokenizer
